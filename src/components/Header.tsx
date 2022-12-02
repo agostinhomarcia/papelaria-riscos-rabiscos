83fd1eb829eea89a434bb0fcf5139ec678d292a1
Marcia Agostinho <marciaagostinho27@gmail.com>
2022-12-02 16:41:23 -0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Container} from 'react-bootstrap'
+
+
+function Header() {
+    return (
+        <Container className='bg-white shadow-sm mb-3'  >
+            <h1 >Papelaria Riscos 3 Rabiscos</h1>
+        </Container>
+    )
+    }
+
+export default Header
